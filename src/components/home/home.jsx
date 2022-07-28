fad91ce0f2e8b5891121803fa5b39324c16bfac3
EniuK <semen1133@wp.pl>
2022-07-28 14:03:25 +0200

    beer

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,5 +1,14 @@
+import { useEffect, useState } from "react";
 import "./home.scss";
 const Home = () => {
+  const [beer, setBeer] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.punkapi.com/v2/beers/random")
+      .then((res) => res.json())
+      .then((data) => setBeer(data));
+  }, []);
+
   return (
     <div className="home-container">
       <div className="about">
@@ -8,9 +17,21 @@ const Home = () => {
         aware of
         <ul>
           <li>Do not refresh site when you are in single recipe</li>
-          <li>App cant delete or update recipes - ask creator to delete/update recipe</li>
-          <h3>I'll fix everything and add new features in future. Just be patient :)</h3>
+          <li>
+            App cant delete or update recipes - ask creator to delete/update
+            recipe
+          </li>
+          <h3>
+            I'll fix everything and add new features in future. Just be patient
+            :)
+          </h3>
         </ul>
+        <h4>Also, I'm gonna train here some features in react. </h4>
+      </div>
+      <div className="fun">
+        {beer.map((b) => {
+          return <img src={`${b.image_url}`} alt="" srcset="" />;
+        })}
       </div>
     </div>
   );
